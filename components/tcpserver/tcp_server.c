91ff89b01dc424dc60444a5aab64b54fd3bbd91e
LarryN3t <lophone75@gmail.com>
2020-01-22 16:23:14 +0100

    fixed wifi bug

--- components/tcpserver/tcp_server.c
+++ components/tcpserver/tcp_server.c
@@ -110,6 +110,8 @@ void initialise_wifi(void)
     ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
     ESP_ERROR_CHECK( esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
     ESP_ERROR_CHECK( esp_wifi_start() );
+    ESP_ERROR_CHECK( esp_wifi_set_ps(WIFI_PS_NONE) );
+    
 }
 
 void wait_for_ip()
